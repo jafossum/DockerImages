dea3e8e2aa24c2e716a42ad76f0da4cefcd9dc47
John A. Fossum <3495141+jafossum@users.noreply.github.com>
2019-03-28 21:47:38 +0000

    Using explicit version tag

--- homeassistant_rpi/upgrade.sh
+++ homeassistant_rpi/upgrade.sh
@@ -1,15 +1,15 @@
 # Pull the latest remote images, restart and optionally clean the setup
 
 # Pull image
-echo "Pulling last image(s) for ${PWD##*/}..."
+echo "Pulling last image(s) for ${PWD}..."
 docker-compose pull
 
 # Stop the running compose setup
-echo "Stopping ${PWD##*/} runtime"
+echo "Stopping ${PWD} runtime"
 docker-compose down
 
 # Start the new compose setup
-echo "Starting ${PWD##*/} runtime"
+echo "Starting ${PWD} runtime"
 docker-compose up -d
 
 # Ask if docker system prune should be performed
